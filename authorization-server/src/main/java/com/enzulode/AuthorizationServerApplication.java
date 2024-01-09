bbec360a41a0d9c92f7c7d44f0dd392a613a5212
Vladislav Andreev <andreevvlad443@gmail.com>
2024-01-09 20:23:44 +0300

    feat: jar-level entrypoint

---
+++ authorization-server/src/main/java/com/enzulode/AuthorizationServerApplication.java
@@ -0,0 +1,20 @@
+package com.enzulode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/** This class contains jar-level application entrypoint. */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AuthorizationServerApplication {
+
+    /**
+     * Jar-level application entrypoint.
+     *
+     * @param args command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+}
