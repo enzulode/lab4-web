a44c5801ad16186abebcbaf26cc39704bec718e2
Vladislav Andreev <andreevvlad443@gmail.com>
2024-01-11 18:03:48 +0300

    feat: disabling rective user details service autoconfig as unnecessary config

--- api-gateway/src/main/java/com/enzulode/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/enzulode/ApiGatewayApplication.java
@@ -2,10 +2,11 @@ package com.enzulode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /** This class contains jar-level application entrypoint. */
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
