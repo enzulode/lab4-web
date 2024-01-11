2a0893b04b88c8f606563c6911833454803deb15
Vladislav Andreev <andreevvlad443@gmail.com>
2024-01-12 00:33:53 +0300

    feat: add builder

--- authorization-server/src/main/java/com/enzulode/dao/entity/UserEntity.java
+++ authorization-server/src/main/java/com/enzulode/dao/entity/UserEntity.java
@@ -1,13 +1,15 @@
-package com.enzulode.entity;
+package com.enzulode.dao.entity;
 
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEntity {
