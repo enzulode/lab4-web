2a214138248508a1c51f9b90767e7c5be7d3a518
Vladislav Andreev <andreevvlad443@gmail.com>
2024-01-12 02:47:10 +0300

    feat: remove unnecessary user details service instantiation

--- authorization-server/src/main/java/com/enzulode/conf/SecurityConfig.java
+++ authorization-server/src/main/java/com/enzulode/conf/SecurityConfig.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /** This class contains security-related configurations for the authorization server. */
@@ -35,26 +31,5 @@ public class SecurityConfig {
                                         .authenticated())
                 .formLogin(Customizer.withDefaults());
         return security.build();
-    }
-
-    /**
-     * This method configures in-memory {@link UserDetailsService} implementation with development
-     * users registered.
-     *
-     * @return {@link UserDetailsService} instance
-     */
-    @Bean
-    public UserDetailsService developmentUsers() {
-        UserDetails defaultUser =
-                User.builder().username("enzulode").password("{noop}dev").roles("USER").build();
-
-        UserDetails adminUser =
-                User.builder()
-                        .username("admin")
-                        .password("{noop}dev")
-                        .roles("USER", "ADMIN")
-                        .build();
-
-        return new InMemoryUserDetailsManager(defaultUser, adminUser);
     }
 }
