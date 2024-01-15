8a9c32dab2456c11a53bc05395f926507c83a13c
Vladislav Andreev <andreevvlad443@gmail.com>
2024-01-15 21:29:33 +0300

    feat: allow requests on oauth2-related endponts & core refactoring

--- authorization-server/src/main/java/com/enzulode/conf/SecurityConfig.java
+++ authorization-server/src/main/java/com/enzulode/conf/SecurityConfig.java
@@ -1,15 +1,31 @@
 package com.enzulode.conf;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 /** This class contains security-related configurations for the authorization server. */
 @Configuration
 public class SecurityConfig {
+
+    /** User details service instance. */
+    private final UserDetailsService userDetailsService;
+
+    /**
+     * Constructs security configuration from provided parameters.
+     *
+     * @param userDetailsService user details instance
+     */
+    public SecurityConfig(
+            @Qualifier("customUserDetailsService") UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
 
     /**
      * This method configures security policy for the whole application.
@@ -21,15 +37,19 @@ public class SecurityConfig {
     @Bean
     @Order(2)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity security) throws Exception {
-        security.authorizeHttpRequests(
-                        auth ->
-                                auth.requestMatchers("/error")
-                                        .permitAll()
-                                        .requestMatchers("/actuator/**")
-                                        .permitAll()
-                                        .anyRequest()
-                                        .authenticated())
-                .formLogin(Customizer.withDefaults());
+        //        @formatter:off
+        security
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/error").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
+                .requestMatchers("/oauth2/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(Customizer.withDefaults());
+
+        security.getSharedObject(AuthenticationManagerBuilder.class)
+            .userDetailsService(userDetailsService);
+//        @formatter:on
         return security.build();
     }
 }
