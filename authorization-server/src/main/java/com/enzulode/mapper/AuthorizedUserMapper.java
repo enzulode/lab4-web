6e60d7ede7f94cedd366aa61da531cc9896166fd
Vladislav Andreev <andreevvlad443@gmail.com>
2024-01-19 03:42:56 +0300

    feat: improve user authorities mapping

--- authorization-server/src/main/java/com/enzulode/mapper/AuthorizedUserMapper.java
+++ authorization-server/src/main/java/com/enzulode/mapper/AuthorizedUserMapper.java
@@ -1,8 +1,13 @@
 package com.enzulode.mapper;
 
+import com.enzulode.dao.entity.AuthorityEntity;
+import com.enzulode.dao.entity.RoleEntity;
 import com.enzulode.dao.entity.UserEntity;
 import com.enzulode.dto.AuthorizedUser;
-import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 /** This class contains authorized user mapping utilities. */
 public class AuthorizedUserMapper {
@@ -14,13 +19,21 @@ public class AuthorizedUserMapper {
      * @return authorized user instance
      */
     public static AuthorizedUser map(UserEntity entity) {
-        return AuthorizedUser.builder(
-                        entity.getEmail(), entity.getPasswordHash(), Collections.emptyList())
-                .id(entity.getId())
-                .firstName(entity.getFirstName())
-                .lastName(entity.getLastName())
-                .middleName(entity.getMiddleName())
-                .profileImg(entity.getProfileImg())
-                .build();
+        //        @formatter:off
+        List<GrantedAuthority> authorities = entity.getRoles().stream()
+            .filter(RoleEntity::getActive)
+            .flatMap(role -> role.getAuthorities().stream())
+            .filter(AuthorityEntity::getActive)
+            .map(authority -> new SimpleGrantedAuthority(authority.getCode()))
+            .collect(Collectors.toList());
+
+        return AuthorizedUser.builder(entity.getEmail(), entity.getPasswordHash(), authorities)
+            .id(entity.getId())
+            .firstName(entity.getFirstName())
+            .lastName(entity.getLastName())
+            .middleName(entity.getMiddleName())
+            .profileImg(entity.getProfileImg())
+            .build();
+//        @formatter:on
     }
 }
