e589c817280578857788d5cd4d79240564ecafcc
Vladislav Andreev <andreevvlad443@gmail.com>
2024-01-22 23:19:25 +0300

    feat: enable cors configuration properties holder

--- dev-resource-server/src/main/java/com/enzulode/DevResourceServerApplication.java
+++ dev-resource-server/src/main/java/com/enzulode/DevResourceServerApplication.java
@@ -1,12 +1,15 @@
 package com.enzulode;
 
+import com.enzulode.config.model.CorsConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /** This class contains jar-level application entrypoint. */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(CorsConfigurationProperties.class)
 public class DevResourceServerApplication {
 
     /**
